b6556fa95d42a099d851b4cf58b2764cf8cd35dc
Marco Gonçalves <marcomatiaspg@gmail.com>
2021-02-28 13:25:42 +0000

    update logger to stop with ctrl + esc

--- pythonLogger/keyboard.py
+++ pythonLogger/keyboard.py
@@ -9,17 +9,31 @@ def on_press(key):
     dict["last_key"] = datetime.datetime.now()
     if key == keyboard.Key.backspace:
         dict["backspace"] += 1
+    elif key == keyboard.Key.ctrl:
+        chaves["ctrl"]=True
+    elif key == keyboard.Key.esc:
+        chaves["esc"]=True
+
 
 
 # deatect key releases
 def on_release(key):
-    if key == keyboard.Key.esc:
+    if chaves["esc"] & chaves["ctrl"]:
         return False
+    elif key == keyboard.Key.esc:
+        chaves["esc"]=False
+    elif key == keyboard.Key.ctrl:
+        chaves["ctrl"]=False
+
 
 dict = {
     "keys":0,
     "backspace":0,
     "last_key": datetime.datetime.now()
+}
+chaves = {
+    "esc":False, 
+    "ctrl":False
 }
 
 url_user = 'http://localhost:3000/api/v1/sign_in'
@@ -39,7 +53,7 @@ if x["is_success"]==True:
         url_work_sessions += str(work_session[0])
         print("Started recording")
         with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-            listener.join()
+                listener.join()
         print("Stopped recording")
         dict["last_key"] = dict["last_key"].isoformat()
         r = requests.put(url_work_sessions, json=dict)
